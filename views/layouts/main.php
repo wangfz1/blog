fd06bce99637d56cc43b5fbc901371f84cb1e8c2
wangfanzhou <wang.fanzhou@nint.com>
2020-06-24 18:15:48 +0800

    update 1

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -41,17 +41,26 @@ $assets = AppAsset::register($this);
 <!--        </ul>-->
 <!--    </div>-->
 
-    <div class="layui-side layui-bg-black" style="margin-top: -60px;width:400px;opacity: 90%">
+    <div class="layui-side layui-bg-black" style="margin-top: -60px;width:400px;opacity: 85%">
         <div class="layui-side-scroll" style="width: 400px;">
             <fieldset class="layui-elem-field layui-field-title" style="margin-top: 30px;">
                 <legend style="color: white">Fanzhou Wang</legend>
             </fieldset>
-            <ul class="layui-nav layui-nav-tree"  style="width: 400px;height:800px;background-image: inherit">
-                <img src = "pics/avatar.jpg" class="layui-circle"  style="width: 320px;height:300px;margin:20px 40px 20px 40px;"/>
+            <ul class="layui-nav layui-nav-tree"  style="width: 400px;height:85%;background-image: inherit">
+                <img src = "pics/avatar.jpg" class="layui-circle"  style="width: 320px;height:38%;margin:20px 40px 20px 40px;"/>
 
-                <li class="layui-nav-item" style="margin:0 0 50px 50px"><a href="" style="font-size: 25px;"><img src="svg/education.svg" style="padding-bottom:5px;height:45px;width:45px"/> Education</a></li>
-                <li class="layui-nav-item" style="margin:0 0 50px 50px"><a href="" style="font-size: 25px"><img src="svg/interest.svg" style="padding-bottom:5px;height:45px;width:45px"/> Interests</a></li>
-                <li class="layui-nav-item" style="margin:0 0 50px 50px"><a href="" style="font-size: 25px"><img src="svg/travel.svg" style="padding-bottom:5px;height:45px;width:45px"/> Travels</a></li>
+                <li class="layui-nav-item" style="margin:0 0 50px 50px"><a href="/education" style="font-size: 25px;"><img src="svg/education.svg" style="padding-bottom:5px;height:100%;width:45px"/> Education</a></li>
+                <li class="layui-nav-item" style="margin:0 0 50px 50px"><a href="/interests" style="font-size: 25px"><img src="svg/interest.svg" style="padding-bottom:5px;height:100%;width:45px"/> Interests</a></li>
+                <li class="layui-nav-item" style="margin:0 0 50px 50px"><a href="/travels" style="font-size: 25px"><img src="svg/travel.svg" style="padding-bottom:5px;height:100%;width:45px"/> Travels</a></li>
+            </ul>
+
+            <ul class="layui-nav layui-row"  style="width: 400px;background-image: inherit">
+                <li class="layui-nav-item">
+                    <a href="/education" style=""><img src="svg/instagram.svg" style="padding-bottom:5px;height:100%;width:45px"/> <b>insta</b></a>
+                </li>
+                <li class="layui-nav-item">
+                    <a href="/education" style=""><img src="svg/webo.svg" style="padding-bottom:5px;height:100%;width:45px"/> <b>webo</b></a>
+                </li>
             </ul>
         </div>
     </div>
