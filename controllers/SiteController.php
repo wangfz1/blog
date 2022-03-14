bb16743a3734a8ba3a3fba310c7e2fb3f6aeb848
fwan3408@uni.sydney.edu.au <Wfz893843380>
2022-03-14 15:17:41 +0800

    1

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -62,7 +62,7 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return json_encode(['code' => 200, 'css' => "fontSize: this.finalSize +'px', color: this.finalColor"]);
+        return json_encode(['code' => 200, 'css' => "fontSize: 100px, color: black"]);
         $result_1 = (new Query())->select('*')->from('member')->all(Yii::$app->db);
         $result_2 = Yii::$app->db->createCommand("select id from member")->queryAll();
 
